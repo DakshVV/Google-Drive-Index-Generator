39026c3caa6d6e0874497061fd1f29b6298a2145
Glitch (hello-express) <none>
2019-10-06 05:19:20 +0000

    🚇🚋 Checkpoint ./package.json:644938/2630 ./server.js:644938/5585 ./views/index.html:644938/138

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const xf = require("xfetch-js");
 
-app.get('/', (req,res)=>{
-  res.sendFile(__dirname + '/views/index.html')
-})
-app.get('/getcode',(req,res)=>{
-  console.log(req)
-})
-app.listen(process.env.PORT)
+const app = express();
+app.use(bodyParser.urlencoded());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+function replace(t, a, b) {
+  const reg = new RegExp(String.raw`(${a}: \').*?(\')`);
+  return t.replace(reg, "$1" + b + "$2");
+}
+app.post("/getcode", async (req, res) => {
+  const p = req.body;
+  console.log(p)
+  const { refresh_token } = await xf
+    .post("https://www.googleapis.com/oauth2/v4/token", {
+      urlencoded: {
+        code: p.auth_code,
+        client_id: "202264815644.apps.googleusercontent.com",
+        client_secret: "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
+        redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+        grant_type: "authorization_code"
+      }
+    })
+    .json();
+  let code = await xf
+    .get(
+      "https://raw.githubusercontent.com/maple3142/GDIndex/master/worker/dist/worker.js"
+    )
+    .text();
+  code = replace("refresh_token", refresh_token);
+  for (const [k, v] of Object.entries(p)) {
+    code = replace(code, k, v);
+  }
+  res.send(code);
+});
+app.listen(process.env.PORT);
